84b7d409f4b26346fa28a5c0c92eb4067a0b4a2d
Brendan Berrigan <brendan.berrigan@p0.dev>
2026-02-24 11:48:02 -0500

    Remove unneeded output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "lambda_execution_role" {
-  description = "P0 connector Lambda service role"
-  value       = aws_iam_role.lambda_execution
-}
-
 output "lambda" {
   description = "P0 connector Lambda"
   value       = aws_lambda_function.p0_connector
